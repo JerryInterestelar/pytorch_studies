99ed6b6121787fa240bacddca87313e83bdb0e2a
JerryInterestelar <jerrydesousa17@gmail.com>
2026-07-20 14:58:41 -0300

    feat: script para classificação de utensilios

---
+++ scripts/image_guesser.py
@@ -0,0 +1,55 @@
+import torch
+import matplotlib.pyplot as plt
+from torchvision.transforms import v2
+from torchvision import datasets
+from torch import Tensor
+
+from basics.neural_network import NeuralNetwork
+
+
+fashion_dataset = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]),
+)
+weights = torch.load("basic_model_weights.pth", weights_only=True)
+
+model = NeuralNetwork()
+model.load_state_dict(weights)
+
+
+def show_image(dataset: datasets.MNIST, index: int = 0):
+    image, label = dataset[index]
+
+    plt.imshow(image.squeeze(), cmap="gray")
+    plt.title(dataset.classes[label])
+    plt.show()
+
+
+def predict_image(
+    dataset: datasets.MNIST,
+    model: NeuralNetwork,
+    index: int,
+) -> int:
+    model.eval()
+
+    image: Tensor
+    image, _ = dataset[index]
+    image_batch = image.unsqueeze(0)
+
+    with torch.no_grad():
+        predicion: Tensor = model(image_batch)
+        predicted_class = int(predicion.argmax(1).item())
+    return predicted_class
+
+
+if __name__ == "__main__":
+    image_index = 0
+    print(
+        "A imagem {} selecionada é: {}".format(
+            image_index,
+            fashion_dataset.classes[predict_image(fashion_dataset, model, image_index)],
+        )
+    )
+    show_image(fashion_dataset, image_index)
