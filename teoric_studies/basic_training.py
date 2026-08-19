e7860821f6a8b83309bd2be5587871f393cf5c09
JerryInterestelar <jerrydesousa17@gmail.com>
2026-08-19 12:29:08 -0300

    feat(teoric): adição de mais pesos e função sigmoid

--- teoric_studies/basic_training.py
+++ teoric_studies/basic_training.py
@@ -1,44 +1,67 @@
 import random
+import math
 import matplotlib.pyplot as plt
 
 
-def treinamento(conjunto: list[tuple[int, int, int]]):
-    delta = 1e-3
-    w_1 = random.random()
-    w_2 = random.random()
+def dsigmoid(x: float) -> float:
+    f = sigmoid(x)
+    return f * (1 - f)
 
-    w_1_samples = []
-    w_2_samples = []
+
+def sigmoid(x: float) -> float:
+    return 1 / (1 + math.exp(-x))
+
+
+def treinamento(conjunto: list[tuple[int, int, int]]):
+    delta = 0.5
+    wa = random.random()
+    wb = random.random()
+    w1 = random.random()
+    w2 = random.random()
+
+    wa_samples = []
+    wb_samples = []
+    w1_samples = []
+    w2_samples = []
     epoca = 0
 
     while epoca < 1000:
-        d_w_1, d_w_2 = 0, 0
+        dw1, dw2 = 0, 0
         random.shuffle(conjunto)
         for linha in conjunto:
-            x_1, x_2, T = linha
+            x1, x2, T = linha
 
             # Propagação
-            R = x_1 * w_1 + x_2 * w_2
+            n1 = x1 * w1 + x2 * w2
+            n2 = n1 * wa + 1 * wb
+            R = sigmoid(n2)
 
             # Retro propagação
 
-            d_w_1 = -(T - R) * x_1
-            d_w_2 = -(T - R) * x_2
+            erro = -(T - R)
+            dwa = erro * dsigmoid(n2) * n1
+            dwb = erro * dsigmoid(n2)
+            dw1 = erro * dsigmoid(n2) * wa * x1
+            dw2 = erro * dsigmoid(n2) * wa * x2
             # Aprendizado
-            w_1 -= delta * d_w_1
-            w_2 -= delta * d_w_2
+            wa -= delta * dwa
+            wb -= delta * dwb
+            w1 -= delta * dw1
+            w2 -= delta * dw2
 
         epoca += 1
         # print(epoca)
         if epoca % 100 == 0:
-            # print(w_1, w_2)
-            w_1_samples.append(w_1)
-            w_2_samples.append(w_2)
-    return w_1_samples, w_2_samples
+            # print(w1, w2)
+            wa_samples.append(wa)
+            wb_samples.append(wb)
+            w1_samples.append(w1)
+            w2_samples.append(w2)
+    return wa_samples, wb_samples, w1_samples, w2_samples
 
 
 if __name__ == "__main__":
-    conjunto = [(0, 0, 0), (0, 1, 1), (1, 0, 1), (1, 1, 0)]
+    conjunto = [(0, 0, 1), (0, 1, 0), (1, 0, 0), (1, 1, 1)]
     pesos = treinamento(conjunto)
 
     for x1, x2, _ in conjunto:
@@ -47,8 +70,10 @@ if __name__ == "__main__":
 
     epocas = list(range(0, 1000, 100))
     _, ax = plt.subplots()
-    ax.plot(epocas, pesos[0], label="w_1")
-    ax.plot(epocas, pesos[1], label="w_2")
+    ax.plot(epocas, pesos[0], label="wa")
+    ax.plot(epocas, pesos[1], label="wb")
+    ax.plot(epocas, pesos[2], label="w1")
+    ax.plot(epocas, pesos[3], label="w2")
     ax.set(
         xlabel="epocas",
         ylabel="valor dos pesos",
