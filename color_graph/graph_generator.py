6b9cf4690e513aa7216cdcbc6104dff046c3495e
JerryInterestelar <jerrydesousa17@gmail.com>
2026-08-26 15:05:56 -0300

    feat(color_graph): geração de grafos direcionais aleatórios

--- color_graph/graph_generator.py
+++ color_graph/graph_generator.py
@@ -1,9 +1,11 @@
-import numpy as np
-from graph_utils import find_all_paths, find_path, find_shortest_path, get_graph_dict  # noqa: F401
+from graph_utils import get_graph_dict, random_graph_edge_list  # noqa: F401
 
 
 def main():
-    np_graph = np.array([[0, 0, 1, 1, 2, 3, 3, 4, 5], [1, 2, 2, 3, 3, 2, 4, 5, 2]])
+    # np_graph = np.array([[0, 0, 1, 1, 2, 3, 3, 4, 5], [1, 2, 2, 3, 3, 2, 4, 5, 2]])
+    # a = {0: [1, 2], 1: [2, 3], 2: [3], 3: [2, 4], 4: [5], 5: [2]}
+    for n, e in random_graph_edge_list(6).T.tolist():
+        print(f"{n} {e}")
 
 
 if __name__ == "__main__":
--- color_graph/graph_utils.py
+++ color_graph/graph_utils.py
@@ -2,6 +2,27 @@ import numpy as np
 from collections import defaultdict
 
 
+def random_graph_matrix(n: int) -> np.ndarray:
+    if n > 100:
+        print("Não foi testado para valores maiores que 100, tente valores menores")
+        return np.array([[0]])
+    random_matrix = np.random.randint(2, size=(n, n))
+    np.fill_diagonal(random_matrix, 0)
+    return random_matrix
+
+
+def random_graph_edge_list(n: int) -> np.ndarray:
+    m = random_graph_matrix(n)
+    u, v = [], []
+    for i, node in enumerate(m):
+        for j, link in enumerate(node):
+            if link != 0:
+                u.append(i)
+                v.append(j)
+
+    return np.array([u, v])
+
+
 def get_graph_dict(graph: np.ndarray) -> dict:
     dict_graph = defaultdict(list)
     for u, v in graph.T.tolist():
@@ -16,6 +37,16 @@ def get_graph_ndarray(graph: dict) -> np.ndarray:
             u.append(node)
             v.append(link)
     return np.array([u, v])
+
+
+def get_graph_matrix(graph: np.ndarray) -> list[list]:
+    m = []
+    n = len(set(graph[0]))
+    for _ in range(n):
+        m.append([0] * n)
+    for n, e in graph.T.tolist():
+        m[n][e] = 1
+    return m
 
 
 def find_path(graph: dict, start, end, path=[]):
