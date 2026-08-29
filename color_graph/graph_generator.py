60395bdf958efe0e0fff04c44d149658b70d0337
JerryInterestelar <jerrydesousa17@gmail.com>
2026-08-29 15:04:21 -0300

    feat(color_graph): datasets - primeira versão da geração de datasets

--- color_graph/graph_generator.py
+++ color_graph/graph_generator.py
@@ -1,17 +1,69 @@
-from graph_utils import Graph
+import random
+import pandas as pd
 
 
-def main():
-    n = 5
-    graph = Graph.random(n)
+from color_graph.color_utils import get_colors
+from color_graph.graph_utils import Graph, GraphStructure
+
+
+def basic_graph(graph_dict: GraphStructure | None = None, colors: list | None = None):
+    if graph_dict and colors:
+        graph = Graph(graph_dict, colors)
+    else:
+        graph = Graph.random(5)
     print(graph)
-    result = graph.map_all_edges(lambda n, e: graph.colors[n] == graph.colors[e])
-    print(result)
-    print("Grafo mestre" if not any(result) else "Grafo comum")
-    print(graph.find_path(0, 4))
+
+    assert graph
+
+    def compare_nodes(node, neighbor):
+        result = graph.colors[node] == graph.colors[neighbor]
+        print(f"Nó {node} -> Vizinho {neighbor}: {'igual' if result else 'diferente'}")
+        return result
+
+    result = graph.map_all_edges(compare_nodes)
+    print("MESTRE" if not any(result) else "Grafo comum")
     graph.show()
-    #
+
+
+def gen_diferent_graph_colors(
+    graph: Graph | None, possible_colors: list | None, len: int
+) -> list[list]:
+    assert graph
+    assert possible_colors
+    rows = []
+    for _ in range(len):
+        new_color = random.choices(possible_colors, k=n)
+        graph.set_colors(new_color)
+        result = graph.map_all_edges(lambda n, e: graph.colors[n] == graph.colors[e])
+        rows.append([graph.colors, 1.0 if not any(result) else 0.0])
+    return rows
+
+
+def save_to_csv(
+    rows: list, slice_point: int, train_file_name: str, test_file_name: str
+):
+    data = []
+    for colors, value in rows:
+        line = []
+        for r, g, b in colors:
+            line.extend([r, g, b])
+        line.append(value)
+        data.append(line)
+
+    df = pd.DataFrame(data)
+    print(f"Incidência de valores 1 e 0: {df.iloc[:, -1].value_counts()}")
+    df[:slice_point].to_csv(train_file_name, index=False, header=False)
+    df[slice_point:].to_csv(test_file_name, index=False, header=False)
+    print(f"CSVs salvos em {train_file_name} e {test_file_name}")
 
 
 if __name__ == "__main__":
-    main()
+    n = 5
+    possible_colors = get_colors(4)
+    graph = Graph.random(n, possible_colors)
+    save_to_csv(
+        gen_diferent_graph_colors(graph, possible_colors, 1000),
+        800,
+        "train.csv",
+        "test.csv",
+    )
